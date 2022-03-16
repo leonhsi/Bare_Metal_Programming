efee7f6f450649af186f1515cdd737042fccbf03
leonhsi <leonhsi111@gmail.com>
2022-03-16 13:44:01 +0800

    finish lab3

---
+++ lab3/inc/time.h
@@ -0,0 +1,8 @@
+void init_timer_queue();
+void enqueue_timer(char *message, int sec);
+void pop_timer();
+void insert_timer();
+void invoke_timer();
+void timeout();
+void add_timer(void (*call_back)(char *fmt, ...), char *message, int sec);
+void setTimeout(char *message, int sec);
---
+++ lab3/inc/utils.h
@@ -0,0 +1,9 @@
+void *simple_malloc(int size);
+int strcmp(char *str1, char *str2);
+int strncmp(const char *s1, const char *s2, int n);
+int memcmp(void *s1, void *s2, int n);
+int strlen(char *str);
+char *strcpy(char *des, char *src);
+char *strncpy(char *des, char *src, int n);
+char *strclear(char *s);
+void swap(int *a, int *b);
